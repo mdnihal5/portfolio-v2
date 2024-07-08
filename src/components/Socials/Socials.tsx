77b96a859259e7ab6b42743bfb3f3e3ee0360e2b
mdnihal5 <mdnihal.private1@gmail.com>
2024-07-08 15:10:57 +0530

    test 2-6

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,5 +1,4 @@
 import { RiDiscordLine, RiGithubLine, RiLinkedinFill } from "react-icons/ri";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 const icons = [
 	{ icon: <RiGithubLine size={28} />, delay: 0, link: "https://github.com/mdnihal5", name: "github" },
@@ -11,18 +10,9 @@ const Socials: React.FC = () => {
 	return (
 		<div className="flex w-30 items-center justify-center mt-14 gap-4 relative">
 			{icons.map((icon, index) => (
-				<TooltipProvider delayDuration={90} key={index}>
-					<Tooltip>
-						<TooltipTrigger>
-							<a href={icon.link} target="_blank" rel="noopener noreferrer" style={{ display: "block" }}>
-								{icon.icon}
-							</a>
-						</TooltipTrigger>
-						<TooltipContent className="bg-purple-300 pointer-events-none">
-							<p className="text-sm">{icon.name}</p>
-						</TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<a href={icon.link} target="_blank" rel="noopener noreferrer" style={{ display: "block" }}>
+					{icon.icon}
+				</a>
 			))}
 		</div>
 	);
