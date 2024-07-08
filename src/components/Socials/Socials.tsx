013de4d56cddb8f59dfec5045eda39c9503b64d7
mdnihal5 <mdnihal.private1@gmail.com>
2024-07-08 15:07:34 +0530

    test 2-5

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,6 +1,5 @@
-import { motion } from "framer-motion";
 import { RiDiscordLine, RiGithubLine, RiLinkedinFill } from "react-icons/ri";
-// import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 const icons = [
 	{ icon: <RiGithubLine size={28} />, delay: 0, link: "https://github.com/mdnihal5", name: "github" },
@@ -12,29 +11,18 @@ const Socials: React.FC = () => {
 	return (
 		<div className="flex w-30 items-center justify-center mt-14 gap-4 relative">
 			{icons.map((icon, index) => (
-				<motion.div
-					key={index}
-					initial={{ y: 0 }}
-					animate={{
-						y: [-13, 0, -13],
-						transition: {
-							duration: 3,
-							repeat: Infinity,
-							delay: icon.delay,
-							ease: "easeInOut",
-						},
-					}}
-					whileHover={{
-						scale: 1.2,
-						transition: {
-							duration: 0.2,
-						},
-					}}
-				>
-					<a href={icon.link} target="_blank" rel="noopener noreferrer" style={{ display: "block" }}>
-						{icon.icon}
-					</a>
-				</motion.div>
+				<TooltipProvider delayDuration={90}>
+					<Tooltip>
+						<TooltipTrigger>
+							<a href={icon.link} target="_blank" rel="noopener noreferrer" style={{ display: "block" }}>
+								{icon.icon}
+							</a>
+						</TooltipTrigger>
+						<TooltipContent className="bg-purple-300 pointer-events-none">
+							<p className="text-sm">{icon.name}</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
 			))}
 		</div>
 	);
