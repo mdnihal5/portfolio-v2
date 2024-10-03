ef1eaed1b8f6bbb875598b8c712c7b77b974b205
mdnihal5 <mdnihal.private1@gmail.com>
2024-10-04 00:53:47 +0530

    added footer

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import { About } from "../About/About"
 import { Blogs } from "../Blogs/Blogs"
 import Projects from "../Projects/Projects"
 import Skills from "../Skills/Skills"
+import Footer from "./Footer"
 const Hero = () => {
 
   return (
@@ -10,6 +11,7 @@ const Hero = () => {
       <Skills/>
       <Projects/>
       <Blogs/>
+      <Footer/>
     </div>
   )
 }
